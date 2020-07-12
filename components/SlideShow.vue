830f77ac7f99d9094b259fe46d4e9c9632d010d9
kensuke goto <kensuke.goto.1981@gmail.com>
2020-07-12 15:48:35 +0900

    追加：スライドショーのコンポーネントを作成・表示

---
+++ components/SlideShow.vue
@@ -0,0 +1,28 @@
+<template>
+  <carousel :per-page="2" :pagenation-enabled="false">
+    <slide :key="0">
+      <p>1</p>
+    </slide>
+    <slide :key="1">
+      <p>2</p>
+    </slide>
+    <slide :key="2">
+      <p>3</p>
+    </slide>
+    <slide :key="3">
+      <p>4</p>
+    </slide>
+  </carousel>
+</template>
+
+<script>
+import Carousel from "vue-carousel/src/Carousel.vue";
+import Slide from "vue-carousel/src/Slide.vue";
+
+export default {
+  components: {
+    Carousel,
+    Slide
+  }
+}
+</script>
